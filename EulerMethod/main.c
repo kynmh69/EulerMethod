e3b9c4fa060d9ff46e48c478ec91e1242f8de62a
kynmh69 <hiroki.kayanuma@gmail.com>
2018-06-21 10:42:09 +0900

    Initial Commit

---
+++ EulerMethod/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EulerMethod
+//
+//  Created by Hiroki Kayanuma on 2018/06/21.
+//  Copyright © 2018 Hiroki Kayanuma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
